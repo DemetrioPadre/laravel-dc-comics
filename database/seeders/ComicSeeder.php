21019fc59686053f43549886693cbfc4fcd35d3d
DemetrioPadre <demetriopadre@gmail.com>
2024-03-26 15:59:37 +0100

    aggiustato l errore di sintassi

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ComicSeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $comic = new Comic;
+
+            $comic->title = $faker->word();
+            $comic->description = $faker->paragraph(1, true);
+            $comic->thumb = $faker->imageUrl(640, 480, 'demetrio', true);
+            $comic->price = $faker->randomFloat(2, 10, 20);
+            $comic->series = $faker->word();
+            $comic->sale_date = $faker->date();
+            $comic->type = $faker->randomElement(['graphic novel', 'comic book']);
+
+            $comic->save();
+        }
     }
 }
